ff7d546e7f7a09877bf74c3cac42061f6f3a46d7
Manel Rodero <manelrodero@users.noreply.github.com>
2021-05-08 16:21:07 +0200

    LanzarX v2.5 (Junio 2016)

--- LanzarX.bat
+++ LanzarX.bat
@@ -11,10 +11,11 @@ pushd "%~pd0"
 :: +---Run	-> El programa o diferentes versiones del programa organizadas por directorios (v1, v4, etc.)
 ::
 
-set scriptversion=2.4
-set scriptdate=3 Junio 2015
+set scriptversion=2.5
+set scriptdate=2 Junio 2016
 set scriptauthor=Manel Rodero
 set nversiones=0
+set run.1=0
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::
@@ -37,7 +38,10 @@ set roamingdir=programax.com
 :: NO MODIFICAR desde aquí, es un código común a todos los scripts
 ::
 
-:: Obtenemos el número de versiones y se inicializa una variable indexada "Run.X"
+:: Si no hay versiones
+if not defined versiones goto instrucciones
+
+:: Obtenemos el número de versiones y se inicializa una variable indexada "run.X"
 for %%v in %versiones% do (
 	set /a numversiones=!numversiones!+1
 	set run.!numversiones!=%%v
@@ -45,16 +49,16 @@ for %%v in %versiones% do (
 
 if /i "%1"=="" goto instrucciones
 if /i "%1"=="0" goto deslinkar
-if /i "%1" leq "%numversiones%" (set vrun=!run.%1!&&goto ejecutar)
+if %1 leq %numversiones% (set vrun=!run.%1!&&goto ejecutar)
 goto instrucciones
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :ejecutar
 set programa=%programa:"=%
-call :linkar %vrun%
-call :precheck %vrun%
+if %numversiones% gtr 1 (call :linkar %vrun%) else (call :linkar)
+if %numversiones% gtr 1 (call :precheck %vrun%) else (call :precheck)
 echo Ejecutando '%titulo% %vrun%' ...
-pushd ".\Run\%vrun%"
+if %numversiones% gtr 1 (pushd ".\Run\%vrun%") else (pushd ".\Run")
 start "" "%programa%"
 popd
 goto final
@@ -98,23 +102,43 @@ goto :eof
 ::
 :: MODIFICAR esta rutina según el programa que se quiera ejecutar (podría estar vacía si no es necesario hacer nada)
 ::
-:: Ejemplo:
+:precheck
+goto :eof
+
+::
+:: Ejemplo 1 (MPC-HC):
 :: - Crear un fichero 'programax.ini' vacío para que el programa escriba la configuración y no use el registro
 :: - Borrar la rama del registro donde guarda la configuración si no es portable
 ::
-:precheck
 if /i "%1"=="" (
-	if not exist ".\Run\programax.ini" (
-		echo Creando fichero programax.ini ...
-		> ".\Run\programax.ini" echo [Settings]
-		reg delete HKEY_CURRENT_USER\Software\ProgramaX /f >nul 2>&1
-	)
+        if not exist ".\Run\mpc-hc.ini" (
+                echo Creando fichero mpc-hc.ini ...
+                > ".\Run\mpc-hc.ini" echo [Settings]
+                >> ".\Run\mpc-hc.ini" echo UpdaterAutoCheck=0
+                reg delete HKEY_CURRENT_USER\Software\Gabest /f >nul 2>&1
+        )
 ) else (
-	if not exist ".\Run\%1\programax.ini" (
-		echo Creando fichero programax.ini ...
-		> ".\Run\%1\programax.ini" echo [Settings]
-		reg delete HKEY_CURRENT_USER\Software\ProgramaX /f >nul 2>&1
-	)
+        if not exist ".\Run\%1\mpc-hc.ini" (
+                echo Creando fichero mpc-hc.ini ...
+                > ".\Run\%1\mpc-hc.ini" echo [Settings]
+                >> ".\Run\%1\mpc-hc.ini" echo UpdaterAutoCheck=0
+                reg delete HKEY_CURRENT_USER\Software\Gabest /f >nul 2>&1
+        )
+)
+goto :eof
+
+::
+:: Ejemplo 2 (Spotify):
+:: - Spotify tiene los ejecutables (Run) directamente en Roaming, por tanto se enlazan
+:: - Borrar las actualizaciones que se hayan descargado
+::
+if not exist .\Run (
+	echo Creando link en .\Run -^> .\Roaming ...
+	mklink /j .\Run .\Roaming > nul
+)
+if exist .\Local\Update (
+	echo Borrando .\Local\Update ...
+	rmdir /s /q .\Local\Update
 )
 goto :eof
 
